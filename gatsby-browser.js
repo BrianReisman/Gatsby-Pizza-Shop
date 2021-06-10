c8d592d4917e0cb1e056bf2c4e1ad6894b58d5e1
Brian Reisman <brianreisman@protonmail.com>
2021-06-10 10:28:58 -0400

    feature/add-global-styles-using-styled-components

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Layout from "./src/components/Layout";
+// import "./src/styles/red.css"; //!Test for css this way
 
 // Allows default wrapping in Layout
 export const wrapPageElement = ({ element, props }) => {
