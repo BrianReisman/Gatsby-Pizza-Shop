28575226d8538dc4d06e9f9863bbf3dfca73e4ce
Brian Reisman <brianreisman@protonmail.com>
2021-06-10 17:16:44 -0400

    add global and layout styles using styled components

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,17 +1,51 @@
 import React from "react";
+import styled from "styled-components";
+import "normalize.css";
+
 import Footer from "./Footer";
 import Nav from "./Nav";
-import "normalize.css";
-import GlobalStyles from '../styles/GlobalStyles' //Doesn't need to wrap children
+import GlobalStyles from "../styles/GlobalStyles"; //Doesn't need to wrap children
+import Typography from "../styles/Typography"; //Doesn't need to wrap children
+import stripes from "../assets/images/stripes.svg";
+
+const SiteBorderStyles = styled.div`
+  max-width: 1000px;
+  margin: 12rem auto 4rem auto;
+  margin-top: clamp(
+    2rem,
+    10vw,
+    12rem
+  ); //this overrides the above property if browser doesn't support.max-width
+  background: white url(${stripes});
+  background-size: 1500px;
+  padding: 5px;
+  padding: clamp(5px, 1vw, 25px);
+  box-shadow: 0 0 5px 3px rgba(0, 0, 0, 0.44);
+  border: 5px solid white;
+  @media (max-width: 1100px) {
+    margin-left: 1.5rem;
+    margin-right: 1.5rem;
+  }
+`;
+
+const ContentStyles = styled.div`
+  background-color: white;
+  padding: 2rem;
+`;
 
 const Layout = ({ children }) => {
   return (
-    <div>
+    <>
+      <Typography />
       <GlobalStyles />
-      <Nav />
-      {children}
-      <Footer />
-    </div>
+      <SiteBorderStyles>
+        <ContentStyles>
+          <Nav />
+          {children}
+          <Footer />
+        </ContentStyles>
+      </SiteBorderStyles>
+    </>
   );
 };
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,59 @@
 import React from "react";
 import { Link, navigate } from "gatsby";
+import styled from "styled-components";
+import Logo from "./Logo";
+
+const NavStyles = styled.nav`
+  margin-bottom: 3rem;
+  .logo{
+    transform: translateY(-25%)
+  }
+  ul {
+    margin: 0;
+    padding: 0;
+    text-align: center;
+    list-style: none;
+    display: grid;
+    grid-template-columns: 1fr 1fr auto 1fr 1fr;
+    grid-gap: 2rem;
+    align-items: center;
+    margin-top: -6rem;
+  }
+  li {
+    --rotate: -2deg;
+    transform: rotate(var(--rotate));
+    order: 1;
+
+    &:nth-child(1) {
+      --rotate: 10deg;
+    }
+    &:nth-child(2) {
+      --rotate: -2.5deg;
+    }
+    &:nth-child(4) {
+      --rotate: 2.5deg;
+    }
+    &:hover{
+      --rotate: 3deg;
+    }
+  }
+  a{
+    font-size: 3rem;
+    text-decoration: none;
+    &:hover{
+      color: var(--red)
+    }
+    /* &[aria-current="page"]{
+      color: var(--red)
+    } */
+  }
+
+}
+`;
 
 const Nav = () => {
-  const goToSliceMasters = () => {
-    // 1. wait for 2 seconds
-    setTimeout(() => {
-      navigate("/slicemasters");
-    }, 2000);
-    // 2. change the page
-  };
   return (
-    <nav>
+    <NavStyles>
       <ul>
         <li>
           <Link to="/">Hot Now</Link>
@@ -19,7 +62,9 @@ const Nav = () => {
           <Link to="/pizza">Pizzas (pizza or pizzas?) Menu</Link>
         </li>
         <li>
-          <Link to="/">LOGO</Link>
+          <Link to="/">
+            <Logo />
+          </Link>
         </li>
         <li>
           <Link to="/slicemasters">SliceMasters</Link>
@@ -28,7 +73,7 @@ const Nav = () => {
           <Link to="/order">Order Ahead!</Link>
         </li>
       </ul>
-    </nav>
+    </NavStyles>
   );
 };
 
