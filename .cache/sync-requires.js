b3af6927196a48fd7f1a8989b0901e196300df42
Brian Reisman <brianreisman@protonmail.com>
2021-06-22 16:52:26 -0400

    create gatsby-node.js and start template for pizza pages

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -11,6 +11,7 @@ exports.components = {
   "component---src-pages-index-js": hot(preferDefault(require("/Users/breisman/Documents/learningCode/gatsby/src/pages/index.js"))),
   "component---src-pages-order-js": hot(preferDefault(require("/Users/breisman/Documents/learningCode/gatsby/src/pages/order.js"))),
   "component---src-pages-pizza-js": hot(preferDefault(require("/Users/breisman/Documents/learningCode/gatsby/src/pages/pizza.js"))),
-  "component---src-pages-slicemasters-js": hot(preferDefault(require("/Users/breisman/Documents/learningCode/gatsby/src/pages/slicemasters.js")))
+  "component---src-pages-slicemasters-js": hot(preferDefault(require("/Users/breisman/Documents/learningCode/gatsby/src/pages/slicemasters.js"))),
+  "component---src-templates-pizza-js": hot(preferDefault(require("/Users/breisman/Documents/learningCode/gatsby/src/templates/Pizza.js")))
 }
 
---
+++ gatsby-node.js
@@ -0,0 +1,35 @@
+import path from "path"; //from Node directly
+
+async function turnPizzasIntoPages({ graphql, actions }) {
+  // 1. get a template for this page
+  const pizzaTemplate = path.resolve("./src/templates/Pizza.js");
+  // 2. query all pizzas
+  const { data } = await graphql(`
+    query {
+      pizzas: allSanityPizza {
+        nodes {
+          name
+          slug {
+            current
+          }
+        }
+      }
+    }
+  `);
+  // 3. loop over each pizza and create a page for that pizza
+  data.pizzas.nodes.forEach((pizza) => {
+    actions.createPage({
+      // what is the url of this new page?
+      path: `pizza/${pizza.slug.current}`,
+      component: pizzaTemplate,
+    });
+  });
+}
+
+export async function createPages(params) {
+  // create pages dynamically
+  // 1. pizzas
+  await turnPizzasIntoPages(params);
+  // 2. toppings
+  // 3. slicemasters
+}
